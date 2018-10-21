23d9c17ac7b45e4d23de7fab7393f6957e5a4526
David Fernandez <morenza@gmail.com>
2018-10-21 16:47:39 +0100

    Added checkboxes handling to forms example

--- src/forms/Elements.jsx
+++ src/forms/Elements.jsx
@@ -3,6 +3,7 @@ class Elements extends React.Component {
         super(props);
         this.handleRadio = this.handleRadio.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleCheckbox =  this.handleCheckbox.bind(this);
         this.state = {
             description: `With the right pattern, applications will be more scalable and easier to maintain.
     If you aspire one day to become a Node.js architect (or maybe you're already one and want to extend your knowledge), this presentation is for you.`,
@@ -25,6 +26,18 @@ class Elements extends React.Component {
         let obj = {}
         obj[event.target.value] = event.target.checked // true
         this.setState({radioGroup: obj})
+    }
+
+    handleCheckbox(event) {
+        let checkboxGroupObj = this._cloneCheckboxGroupPart(this.state.checkboxGroup)
+        checkboxGroupObj[event.target.value] = event.target.checked // true
+        this.setState({checkboxGroup: checkboxGroupObj})
+    }
+
+    _cloneCheckboxGroupPart(stateCheckboxGroupPart) {
+        let checkboxGroupObj = {}
+        Object.assign(checkboxGroupObj, stateCheckboxGroupPart) // cloning to avoid modifying state directly
+        return checkboxGroupObj;
     }
 
     handleSubmit(event) {
@@ -52,6 +65,46 @@ class Elements extends React.Component {
                 <input type="radio" name="radioGroup" value='polymer' checked={this.state.radioGroup['polymer']} onChange={this.handleRadio}/>
                 Polymer
             </label>
+            <hr/>
+            <label>
+          <input
+            type="checkbox"
+            name="checkboxGroup"
+            value='node'
+            checked={this.state.checkboxGroup['node']}
+            onChange={this.handleCheckbox}/>
+          Node
+        </label>
+        <br/>
+        <label>
+          <input
+            type="checkbox"
+            name="checkboxGroup"
+            value='react'
+            checked={this.state.checkboxGroup['react']}
+            onChange={this.handleCheckbox}/>
+          React
+        </label>
+        <br/>
+        <label>
+          <input
+            type="checkbox"
+            name="checkboxGroup"
+            value='express'
+            checked={this.state.checkboxGroup.express}
+            onChange={this.handleCheckbox}/>
+          Express
+        </label>
+        <br/>
+        <label>
+          <input
+            type="checkbox"
+            name="checkboxGroup"
+            value='mongodb'
+            checked={this.state.checkboxGroup['mongodb']}
+            onChange={this.handleCheckbox}/>
+          MongoDB
+        </label>
         </form>
        )
     }
